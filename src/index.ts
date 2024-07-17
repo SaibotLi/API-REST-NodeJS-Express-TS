a677e5b4f6c3e9ff4657cc5f7e92a419b2c03eed
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-07-17 15:26:27 -0300

    Route works

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express"
+
+import diaryRouter from "./routes/diaries"
+
 const app = express()
 app.use(express.json()) // Middleware que transforma la req.body a un json
 
@@ -8,6 +11,8 @@ app.get("/ping", (_req, res) => {
     console.log("Someone pinged here!!")
     res.send("pong")
 })
+
+app.use("/api/diaries", diaryRouter)
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
