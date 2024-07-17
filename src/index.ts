ff8086022cf0d82612da18bff6211b4cad4a143c
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-07-17 10:46:09 -0300

    Dependencies added

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+const app = express()
+app.use(express.json()) // Middleware que transforma la req.body a un json
+
+const PORT = 3000
+
+app.get("/ping", (req, res) => {
+    console.log("Someone pinged here!!")
+    res.send("pong")
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
